8de8fe3f9c41d31d9b4744a42189f0f677375f16
Ijeoma Emeruwa <helloijeoma@gmail.com>
2022-06-18 16:48:22 +0100

    project updates

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,8 +4,8 @@ import blockchain from '../assets/images/kiwi.svg'
 import bridge from '../assets/images/bridgecard.svg'
 import staple from '../assets/images/staple.svg'
 import ferry from '../assets/images/ferry.svg'
-import marble from '../assets/images/marble.svg'
-import asteria from '../assets/images/asteria.png'
+// import marble from '../assets/images/marble.svg'
+// import asteria from '../assets/images/asteria.png'
 
 
 export const PROJECTS = [
@@ -46,22 +46,22 @@ export const PROJECTS = [
       subtitle: "Helping businesses track revenue and subscriptions",
       lead: "Read Case Study"
    },
-   {
-      id: 5,
-      project: "Marble Wallet",
-      image: marble,
-      link: "/marble-wallet",
-      title: "Marble Wallet",
-      subtitle: "Simplifying crytocurrency trading",
-      lead: "Read Case Study"
-   },
-   {
-      id: 6,
-      project: "Asteria",
-      image: asteria,
-      link: "/asteria",
-      title: "Asteria",
-      subtitle: "Automated Asset Management System",
-      lead: "Read Case Study"
-   },
+   // {
+   //    id: 5,
+   //    project: "Marble Wallet",
+   //    image: marble,
+   //    link: "/marble-wallet",
+   //    title: "Marble Wallet",
+   //    subtitle: "Simplifying crytocurrency trading",
+   //    lead: "Read Case Study"
+   // },
+   // {
+   //    id: 6,
+   //    project: "Asteria",
+   //    image: asteria,
+   //    link: "/asteria",
+   //    title: "Asteria",
+   //    subtitle: "Automated Asset Management System",
+   //    lead: "Read Case Study"
+   // },
 ]
