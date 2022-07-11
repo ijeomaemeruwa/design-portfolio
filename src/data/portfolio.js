96e18b6bd9110e106fd07163459e126ee1556805
Ijeoma Emeruwa <helloijeoma@gmail.com>
2022-07-11 20:24:11 +0100

    update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -19,24 +19,24 @@ export const PROJECTS = [
       subtitle: "Business management on autopilot",
       lead: "COMING SOON"
    },
-   // {
-   //    id: 2,
-   //    project: "Bridgecard",
-   //    image: bridge,
-   //    link: "/bridgecard",
-   //    title: "Bridgecard",
-   //    subtitle: "One card to manage finances and payments",
-   //    lead: "Read Case Study"
-   // },
-   // {
-   //    id: 3,
-   //    project: "KIWI",
-   //    image: blockchain,
-   //    link: "/kiwi",
-   //    title: "KIWI",
-   //    subtitle: "A smarter way to invest in crypto",
-   //    lead: "Read Case Study"
-   // },
+   {
+      id: 2,
+      project: "Bridgecard",
+      image: bridge,
+      link: "/bridgecard",
+      title: "Bridgecard",
+      subtitle: "One card to manage finances and payments",
+      lead: "Read Case Study"
+   },
+   {
+      id: 3,
+      project: "KIWI",
+      image: blockchain,
+      link: "/kiwi",
+      title: "KIWI",
+      subtitle: "A smarter way to invest in crypto",
+      lead: "Read Case Study"
+   },
    {
       id: 4,
       project: "Staple",
